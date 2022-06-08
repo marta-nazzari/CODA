c06e482a131e02b52a051e6ab9d0f763e4eb4add
marta-nazzari <78352456+marta-nazzari@users.noreply.github.com>
2022-06-08 15:53:35 +0200

    Add files via upload

---
+++ merge_mirge_files.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(magrittr)
+
+args = commandArgs()
+
+# read text file with all miRge3.0 files
+file_list = scan(args[1], what = character())
+
+# create table with first miRNA file as column
+mir_counts = read.csv(file_list[1], header = T)
+
+# append other miRNA files recursively
+for (i in seq(2, length(file_list))) {
+	file_sample = read.csv(file_list[i], header = T)
+	
+	mir_counts = full_join(mir_counts, file_sample, by = 'miRNA')
+}
+
+mir_counts %<>% tibble::column_to_rownames(., 'miRNA')
+
+# save table
+write.table(mir_counts, file = args[2], quote = F, sep = '\t')
